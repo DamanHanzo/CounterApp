28e97902220d4adba0e0beab1d91c8e50489cb7d
damanhanzo <singhdamanpreet1234@gmail.com>
2018-10-04 20:32:26 -0400

    initial migrations and added db connection

---
+++ Migrations/20181005001943_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CounterApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Counters",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Counters", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Counters");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CounterApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CounterApp
 {
@@ -33,6 +35,10 @@ namespace CounterApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CounterContext>
+            (options => options.UseSqlite(this.Configuration.GetConnectionString("appDbConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
